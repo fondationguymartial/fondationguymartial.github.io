70afbbfb08843b3adfb59e671bd6f233c7e2860c
NGOWA <ak56230@etsmtl.net>
2017-07-26 14:47:24 -0400

    add
    guy ajoute fichier

---
+++ A PROPOS DE .php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>A propos de la Fondation Guy Martial</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+    /* Remove the navbar's default margin-bottom and rounded borders */
+   .navbar {
+      margin-bottom: 0;
+      border-radius: 0;
+	  margin-top: 0;
+	  background-color: #000000;
+    }
+    
+    /* Add a gray background color and some padding to the footer */
+    footer {
+      background-color: #f2f2f2;
+      padding: 25px;
+    } 
+    
+  .carousel-inner img {
+      width: 100%; /* Set width to 100% */
+      margin: auto;
+      min-height:200px;
+  }
+
+  /* Hide the carousel text when the screen is less than 600 pixels wide */
+  @media (max-width: 600px) {
+    .carousel-caption {
+      display: none; 
+    }
+  }
+  </style>
+</head>
+<body>
+<a class="logo" href="SITE WEB.html"><img src="logofondationguy.png" class="img-responsive"></a>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+	  <li><a href="SITE WEB.html"></a></li>
+	  <li><a href="SITE WEB.html"></a></li>
+	  <li><a href="SITE WEB.html"></a></li>
+	  <li><a href="SITE WEB.html"></a></li><li><a href="SITE WEB.html"></a></li>
+	  <li><a href="SITE WEB.html"></a></li>
+	  <li><a href="SITE WEB.html"></a></li>
+	  <li><a href="SITE WEB.html"></a></li>
+	  
+      <li><a href="index.php">ACCUEIL</a></li>
+	  <li><a href="PAGEDERREUR.php">FUTUR ELEVE</a></li>
+        <li><a href="PAGEDERREUR.php">ELEVE ACTUEL</a></li>
+        <li><a href="PAGEDERREUR.php">PROGRAMME D'ETUDE</a></li>
+		
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<div class="container">
+<div class="row">
+<div class="col-sm-4">
+<h2><strong>Notre histoire:</strong><img src="histoire.jpg" alt="name"HEIGHT="300PX" WIDTH="300PX"/> </h2>
+</div>
+<div class="col-sm-8">
+<br /><br /><br /><br /><br /><br /><br /><br /><br/>
+
+<h4>En 199X, fils de Dorothée FEMEGNE épouse NZALI et de Raymond NZALI, Guy Martial est né Infirme Moteur Cérébral. Cette déficience entraina un certain combat d'insertion au milieu famillial et par la suite academique. 
+<br/> Guy Martial a commencé par la maternelle  comme tout autre enfant mais a rencontré l'inacceptation par son entrourage  car les maitresses n'étaient pas formées pour prendre en charge ces enfants à besoins spécifiques et les parents des autres enfants s'opposaient à une cohabitation entre leurs enfants et Guy Martial. Il fit donc 1 an dans un centre specialisé dans la rééducation (orthophonie , art plastique, ergonomie....)de la place. Il fit  l'école academique des sourds-muets apres avoir refusé de repartir dans ce centre. Aprés deux ans, sa mere décide de l'enlever dans cette école et de l'insérer dans une école primaire sous évaluation du directeur. 
+<br/>Encadré par ses enseignants, ses amis et  sa famille, en particulier par sa mère qui avait tout abandonné depuis la naissance  de ce dernier  pour le suivre, Guy Martial a obtenu son CEPE(Certiicat d'Etude Primaire et Elementaire).      
+<br/>Au lendemain de l'obtention de son C.E.P.E, Guy Martial presque effondré, demanda à sa mère: "Que sont devenus mes amis que j'avais  cotoyés lors de mon passage dans ce centre de rééducation? Ont-t-il eu leur diplôme?". Elle ne lui donna  pas de réponse fixe. Et il demanda à  sa mère d'ouvrir une école pour ces enfants. Elle repondit qu'elle ne sait pas si elle va être capable.
+<br/>Malgré ses formations reçues  dans les differents seminaires qu'elle participait déjà, motivée par les livres déjà lus, madame NZALI est repartie à l'école et a suivi une formation en psychomotricité. 
+<br/>Finalement en décembre 2005, elle exauça  le voeux de son fils, c'est à dire, ouvrir une école  donc la mission première est de remédier à l’erreur des centres de rééducation camerounaise et dans certains pays du monde qui oublient souvent le côté intellectuel qui doit être illuminé de sciences ou encore connaissances  contribuant ainsi à l’indépendance totale de la personne.   
+</h4>
+</div>
+</div>
+<div class="row">
+<div class="col-sm-4">
+<h2><strong>Nous sommes:</strong><img src="representation.jpg" alt="name"HEIGHT="300PX" WIDTH="300PX"/> </h2>
+</div>
+<div class="col-sm-8">
+<br /><br /><br /><br /><br /><br /><br />
+<h4>
+La Fondation Guy Martial est présententement en restructuration. Nous avons : <br/><br/>- une École (Guy Martial Center) qui regorge une dizaine d'employers  motivés et devoués( des Enseignants formés par l' ENIEGE , educateurs specialisés, psychomotriciens, ergothérapeutes, les psyhcologues ...) 
+<br/><br/> - Une assocciation: Association d'Espoir des Deficients Intellectuls du Cameroun l'ASSEDIC qui defend les droits de la personne deficiente intellectuelle et organise les activités d'insertion à tout niveau possible. </h4></div>
+
+</div>
+<div class="row">
+<div class="col-sm-4">
+<br/><br/>
+<h2><strong>Notre objectif:</strong><img src="Objectifs.jpg" alt="name"HEIGHT="300PX" WIDTH="300PX"/> </h2>
+</div>
+<div class="col-sm-8">
+<br /><br /><br /><br /><br /><br /><br /><br />
+
+<h4> En collaboration avec la délégation des affaires sociales  et la délégation de l’Education de bases au Cameroun et intervenant dans l’éducation intégrale de l’enfant, la Fondation Guy Martial  se veut révolutionnaire. 
+Son objectif est de fournir à chaque enfant une totale confiance en soi, une ouverture d’esprit et une indépendance qui sont  les éventails indispensables dans ce monde  en plein mutation. C’est ainsi que N.N.Guy Martial disait : « Apprendre pour être libre car la connaissance est une liberté qu’on acquiert. »
+<br /> <br/>Elle a pour mission l'intégration totale des déficients intellectuels(Infirme Moteur Cerebrale(I.M.C), Autiste, Trisomie et les enfants possedant des Troubles de  Deficit de l’Attention avec ou sans Hyperactivité (TDAH ) ) dans les familles, dans les activités scolaires, dans les loisirs et voir le monde du travail avec les soit disant "normaux".
+</h4>
+</div>
+</div>
+
+<div class="row">
+<div class="col-sm-4">
+<br /><br />
+<h2><strong>Nos references:</strong><br /><img src="horloge.png" alt="name"HEIGHT="300PX" WIDTH="300PX"/></h2>
+</div>
+<div class="col-sm-8">
+
+<br /><br /><br /><br /><br /><br /><br /><br /><br />
+
+<h4>
+
+<br />
+
+- La thérapeute en chef de la  Fondation Guy Martial de la personne de madame FEMEGNE Dorothé epouse NZALI a éduqué son  fils né Infirme Moteur  Cérébral  qui,  aujourd’hui, est en cycle d’ingénierie Informatique – Genie Logiciel. 
+<br /><br/>
+-  Elle  contribue dans la  revendication de l’avènement de la psychomotricité au Cameroun dans  de nombreux séminaires  participés dans les années 199X à 2002 
+<br /><br/>- elle est un membre actif de l’A.M.E.H.M (Association  des Mères des Enfants Handicapés Mentaux) depuis 199X.    
+<br /><br/>- En 2003, elle  réclame déjà le droit à la scolarisation  des personnes Déficientes Intellectuelles (« leur handicap  doit être mentionné sur les fiches de candidature du C.E.P.E »).  Elle continuera dans les autres examens comme les BEPEC, Probatoire, BACC. Cette mention est aujourd’hui l’une de ses victoires en termes de droits des D.I. au Cameroun.
+<br /><br/>- Elle  relève le niveau des enfants présentant les uns, des troubles d’apprentissage, et les autres, une déficience intellectuelle.  Donnant ainsi  une totale confiance en soi à ces personnes. 2003-2005  
+<br /><br/>- En 2005, Mme NZALI Dorothée présente un de ses eleves  au CEP. Il réussit et devient collégien.
+<br /><br/>- La Fondation Guy Martial est créée en 2005, avec  pour objectif de remédier à l’erreur des centres de rééducation Camerounaise qui oublie souvent le coté intellectuel qui doit être illuminé de sciences ou encore connaissances  contribuant ainsi à l’indépendance totale de la personne.
+<br /><br/>- La Fondation  Guy Martial devient la première école camerounaise privée à faire dans l’intégration totale des enfants déficients intellectuelles avec les enfants normaux depuis 2009
+<br /><br/>- Les enfants  de la fondation  ont remportés plus de 6 Médailles or, argent et bronze lors des trois précédents jeux de Spécial-Olympic Cameroun de 2009-2010-2011.
+<br /><br/>- Les enfants de la Fondation remportent le prix du meilleur poème lors de la célébration de la fête de l’enfant africain le 16 juin 2010.
+<br /><br/>- ils en ressortent 6ième du classement dans la participation de la fête de 16 juin 2013. 
+ <br /><br/>- La FGM est représentée fortement, dans la FESCADI (Fédération  Sportive Camerounaise pour Défi cients intellectuels) par :
+<br /><br/>La thérapeute en chef : Mme NZALI Dorothée (trésorière de la ligue régionale littorale)
+<br /><br/>L’Ambassadeur de la FGM : NGOWA NZALI Guy Martial (Délégué régional  du Littoral) (membre du comité paralympique)
+<br /><br/>- Depuis Aout 2013, la F.G.M est en partenariat d’échange  avec la coopérative Espace-Temps au Canada Québec à Montréal.
+<br /><br/>- En 2014, dans le souci de faciliter l’intégration des personnes vivant avec un handicap en milieu scolaire et professionnel, la F.G.M envisage  d'importer  des technologies adéquates pour leur adaptation . (Smart Pen, ordinateur avec logiciels adéquats…)
+</h4>
+</div>
+
+<br/><br/><br/>
+
+</div>
+  
+  
+</div>
+<footer class="container-fluid text-center">
+  <p> <strong>BP 8982 Douala, Cameroun, Bonamoussadi-Denver  +237 699 94 47 17 </strong> </p>
+   <p><h6>Copyrigth  G.M. NGOWA NZALI - Juillet 2017 </h6> </p>
+</footer>
+
+</body>
+</html>
